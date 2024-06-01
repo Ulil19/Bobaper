10a054ab5a052bca1341236d4682d1421a4f5251
Romaria Lastria <lastriaromaria58@gmail.com>
2024-06-01 20:34:16 +0700

    contact

--- app.py
+++ app.py
@@ -44,6 +44,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 # -------------------------------------------  START ADMIN ROUTES ------------------------------------------------------#
 
