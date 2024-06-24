208ae7de4ffa75da8695d62ee0fc76dd4393b594
Rikivos <rizkypenpenz@gmail.com>
2024-06-25 02:33:20 +0700

    perbaikan navbar pada tampilan hp

--- app.py
+++ app.py
@@ -1109,4 +1109,4 @@ def logoutuser():
 
 # --------------------------------------END USER ROUTES----------------------------------------------------#
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5001, debug=True)
+    app.run("0.0.0.0", port=5000, debug=True)
